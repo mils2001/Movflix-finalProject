798a865bc46b2f9653472d332d7b49daf4e16823
chris <Mileschris466@gmail.com>
2024-12-08 12:52:43 +0300

    routes update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import React, {BrowserRouter,Routes,Route} from "react-router-dom"
 import Navbar from './components/Navbar'
 import Flix from './components/flix';
+import Home from './components/Pages/Home'
+import About from './components/Pages/About'
 
 
 
@@ -12,7 +14,8 @@ function App() {
       <Navbar/>
       <Flix/>
       <Routes>
-      
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
       </Routes>
       </BrowserRouter>
     
