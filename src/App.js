2cc92a00afb4947ba3b0b854d7b2c8d84c111666
chris <Mileschris466@gmail.com>
2024-12-20 16:55:13 +0300

    routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import React, {BrowserRouter,Routes,Route} from "react-router-dom"
 import Navbar from './components/Navbar'
 import Flix from './components/flix';
 
@@ -9,11 +8,11 @@ import Flix from './components/flix';
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+    
       <Navbar/>
       <Flix/>
       
-      </BrowserRouter>
+    
     
     </div>
   );
