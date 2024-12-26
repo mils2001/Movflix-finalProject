edd07dde5711e7b3914ab7d214b72b0b34e416ef
chris <Mileschris466@gmail.com>
2024-12-26 11:51:59 +0300

    debugging router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./Navbar";
+import Navbar from "./components/Navbar";
 import Flix from './components/flix'
-import Home from "./Pages/Home";
-import About from "./Pages/About";
-import Contact from "./Pages/Contact";
+import Home from "./components/Pages/Home";
+import About from "./components/Pages/About";
+import Contact from "./components/Pages/Contact";
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <Flix/>
+      
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Flix />} />
+        <Route path="/about" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
